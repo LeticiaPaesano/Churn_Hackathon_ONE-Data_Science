c5263f803565631227a17faec8b2d90508ec4673
Letícia Paesano <leticiapaesano@gmail.com>
2025-12-24 15:24:00 +0000

    chore: cleanup project structure

--- app/main.py
+++ app/main.py
@@ -11,7 +11,11 @@ from typing import Literal
 # =========================================================
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "model", "model.joblib")
+
+MODEL_PATH = os.getenv(
+    "MODEL_PATH",
+    os.path.join(BASE_DIR, "..", "models", "model.joblib")
+)
 
 app = FastAPI(
     title="ChurnInsight API",
